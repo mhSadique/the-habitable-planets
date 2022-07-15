7b252a010643ba3f7efb3a8f1c28a449ae1ca477
Sadique <mhsadique1307@gmail.com>
2022-07-15 09:17:54 +0600

    all done

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 const { parse } = require('csv-parse');
 const fs = require('fs');
 
-const results = [];
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+    return planet['koi_disposition'] === 'CONFIRMED'
+        && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+        && planet['koi_prad'] < 1.6
+}
 
 fs.createReadStream('kepler_data.csv')
-    .on('data', data => {
-        results.push(data);
-        console.log(data);
+    .pipe(parse({
+        comment: '#',
+        columns: true
+    }))
+    .on('data', data => { // the data is coming as a series as JavaScript object
+        if (isHabitablePlanet(data)) {
+            habitablePlanets.push(data);
+        }
     })
     .on('error', err => {
         console.log(err);
     })
     .on('end', () => {
-        console.log(results);
-        console.log('done');
+        // console.log(`${habitablePlanets.length} <hab></hab>itable planets found!`);
+        console.log(habitablePlanets.map(planet => {
+            return planet['kepler_name']
+        }));
     })
